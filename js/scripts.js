5de673809a4f184c4045a45bb44a235c8b5e3a66
chetan6780 <crp20802@gmail.com>
2021-12-02 00:24:37 +0530

    javascripts half part done

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissor = document.getElementById('scissors');
+const playerSign = document.getElementById('player-sign');
+const computerSign = document.getElementById('computer-sign');
+
+rock.onclick = () => {
+    playerSign.className = "";
+    playerSign.className = "fas fa-hand-rock";
+}
+paper.onclick = () => {
+    playerSign.className = "";
+    playerSign.className = "fas fa-hand-paper";
+}
+scissor.onclick = () => {
+    playerSign.className = "";
+    playerSign.className = "fas fa-hand-scissors";
+}
+playerSign.onclick = () => {
+    console.log("player")
+
+}
+computerSign.onclick = () => {
+    console.log("computer")
+}
+
+// -----------------------------------------------------------------------
+
 playRound = (playerSelection, computerSelection) => {
 
     playerSelection = playerSelection.toLowerCase();
@@ -30,6 +58,8 @@ computerPlay = () => {
     const computerOptions = ['rock', 'paper', 'scissors']
     const choiceNumber = Math.floor(Math.random() * 3);
     const computerSelection = computerOptions[choiceNumber];
+    computerSign.className = "";
+    computerSign.className = `fas fa-hand-${computerSelection}`;
     return computerSelection;
 }
 
@@ -37,8 +67,9 @@ game = () => {
     let playerPoints = 0;
     let computerPoints = 0;
     for (let i = 0; i < 2; i++) {
-        const playerSelection = prompt('Rock, paper, scissors: ');
+        // const playerSelection = prompt('Rock, paper, scissors: ');
         const computerSelection = computerPlay();
+
         let roundResult = playRound(playerSelection, computerSelection);
         let result = roundResult[0];
         let playerWins = roundResult[1];
@@ -54,3 +85,4 @@ game = () => {
 }
 
 game();
+
