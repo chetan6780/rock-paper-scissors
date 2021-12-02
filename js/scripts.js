67994c2dc08fed96ab9b8f0cfe384206639eb4db
chetan6780 <crp20802@gmail.com>
2021-12-02 12:01:41 +0530

    history added

--- js/scripts.js
+++ js/scripts.js
@@ -3,56 +3,17 @@ const paper = document.getElementById('paper');
 const scissor = document.getElementById('scissors');
 const playerSign = document.getElementById('player-sign');
 const computerSign = document.getElementById('computer-sign');
+const flexContainer = document.getElementsByClassName('flex-container');
+const message = document.querySelector('.message');
+const historyUpdate = document.querySelector('.history__update');
 
-rock.onclick = () => {
-    playerSign.className = "";
-    playerSign.className = "fas fa-hand-rock";
-}
-paper.onclick = () => {
-    playerSign.className = "";
-    playerSign.className = "fas fa-hand-paper";
-}
-scissor.onclick = () => {
-    playerSign.className = "";
-    playerSign.className = "fas fa-hand-scissors";
-}
-playerSign.onclick = () => {
-    console.log("player")
+var playerPoints = 0;
+var computerPoints = 0;
 
-}
-computerSign.onclick = () => {
-    console.log("computer")
-}
-
+console.log("running...");
+console.log(historyUpdate);
 // -----------------------------------------------------------------------
 
-playRound = (playerSelection, computerSelection) => {
-
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
-            return ["You lose! Paper beats rock.", false];
-        } else {
-            return ["You win! Rock beats scissors.", true];
-        }
-    } else if (playerSelection === 'paper') {
-        if (computerSelection === 'scissors') {
-            return ["You lose! Scissors beats paper.", false];
-        } else {
-            return ["You win! Paper beats rock.", true];
-        }
-    } else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
-            return ["You lose! Rock beats scissors.", false];
-        } else {
-            return ["You win! Scissors beats paper.", true];
-        }
-    }
-}
 
 computerPlay = () => {
     const computerOptions = ['rock', 'paper', 'scissors']
@@ -60,29 +21,82 @@ computerPlay = () => {
     const computerSelection = computerOptions[choiceNumber];
     computerSign.className = "";
     computerSign.className = `fas fa-hand-${computerSelection}`;
+    console.log(`computer - ${computerSelection}`);
     return computerSelection;
 }
 
-game = () => {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    for (let i = 0; i < 2; i++) {
-        // const playerSelection = prompt('Rock, paper, scissors: ');
-        const computerSelection = computerPlay();
+playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
-        let roundResult = playRound(playerSelection, computerSelection);
-        let result = roundResult[0];
-        let playerWins = roundResult[1];
-        (playerWins) ? playerPoints++ : computerPoints++;
-    }
-    if (playerPoints > computerPoints) {
-        console.log('player wins');
-    } else if (playerPoints < computerPoints) {
-        console.log('computer wins');
-    } else {
-        console.log("it's tie");
+    if (playerSelection === computerSelection) {
+        return "<b><pre>It's a tie! </pre></b>";
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            computerPoints++;
+            return "<b><pre>You lose! </pre></b> Paper beats rock.";
+        } else {
+            playerPoints++;
+            return "<b><pre>You win! </pre></b> Rock beats scissors.";
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            computerPoints++;
+            return "<b><pre>You lose! </pre></b> Scissors beats paper.";
+        } else {
+            playerPoints++;
+            return "<b><pre>You win! </pre></b> Paper beats rock.";
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            computerPoints++;
+            return "<b><pre>You lose! </pre></b> Rock beats scissors.";
+        } else {
+            playerPoints++;
+            return "<b><pre>You win! </pre></b> Scissors beats paper.";
+        }
     }
 }
 
-game();
 
+gameWinner = () => {
+    console.log(`player points: ${playerPoints}`);
+    console.log(`computer points: ${computerPoints}`);
+    if (playerPoints === 5 || computerPoints === 5) {
+        if (playerPoints > computerPoints) {
+            console.log('player wins');
+        } else if (playerPoints < computerPoints) {
+            console.log('computer wins');
+        } else {
+            console.log("it's tie");
+        }
+    }
+}
+
+playGame = (sign) => {
+    playerSign.className = "";
+    playerSign.className = `fas fa-hand-${sign}`;
+    console.log(`Player - ${sign}`);
+    let roundResult = playRound(sign, computerPlay());
+    console.log(roundResult);
+    let div = document.createElement("div");
+    div.className = "history";
+    div.innerHTML = message.innerHTML;
+    historyUpdate.prepend(div);
+    console.log(historyUpdate);
+    message.innerHTML = roundResult;
+    gameWinner();
+}
+
+game = () => {
+    flexContainer[0].addEventListener('click', (e) => {
+        console.log(e.target.id);
+        const sign = e.target.id;
+        playGame(sign);
+    });
+}
+
+// game => playGame(sign) => playRound(playerSelection, computerSelection) 
+// => gameWinner();
+
+game();
